e84592321ce656fa1ae53a3f974775b8848156ac
Matheus Costa <matheuscosta2204@gmail.com>
2020-08-18 11:39:58 -0300

    Updates

---
+++ precache-manifest.7c41d27090d86509e587e7b421fd61be.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e04961f1c182b1590b403a51532ba43c",
+    "url": "/luiza-confeitaria/index.html"
+  },
+  {
+    "revision": "482d4586deb02a3cd4b9",
+    "url": "/luiza-confeitaria/static/css/2.1f08eb9e.chunk.css"
+  },
+  {
+    "revision": "548cacf95f44bb3e7ae2",
+    "url": "/luiza-confeitaria/static/css/main.02b4d084.chunk.css"
+  },
+  {
+    "revision": "482d4586deb02a3cd4b9",
+    "url": "/luiza-confeitaria/static/js/2.ff700a03.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/luiza-confeitaria/static/js/2.ff700a03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "548cacf95f44bb3e7ae2",
+    "url": "/luiza-confeitaria/static/js/main.2ba6ac21.chunk.js"
+  },
+  {
+    "revision": "df5c0977a37787c6065a",
+    "url": "/luiza-confeitaria/static/js/runtime-main.d8a4d203.js"
+  },
+  {
+    "revision": "6b514a665005debedeac60c6c5b0460f",
+    "url": "/luiza-confeitaria/static/media/chocolate.6b514a66.svg"
+  },
+  {
+    "revision": "54f82c7c9d4ec4f4f3ca30c3a245951d",
+    "url": "/luiza-confeitaria/static/media/fire.54f82c7c.svg"
+  },
+  {
+    "revision": "0d738eaa2f2d2b90d2d3c8f5932c0930",
+    "url": "/luiza-confeitaria/static/media/fruits.0d738eaa.svg"
+  },
+  {
+    "revision": "3eaaa6e5625f3e000b2cab2c23cf6684",
+    "url": "/luiza-confeitaria/static/media/left-arrow.3eaaa6e5.svg"
+  },
+  {
+    "revision": "7ab84698d27a5734c4fdc726a08236c1",
+    "url": "/luiza-confeitaria/static/media/logo-fundo-branco.7ab84698.png"
+  },
+  {
+    "revision": "dbaa2cdd66362d7ac00a669b87b2bff9",
+    "url": "/luiza-confeitaria/static/media/pexels-daniela-echavez-1120970.dbaa2cdd.jpg"
+  },
+  {
+    "revision": "3a817ddc73249b1ed4f068de43026773",
+    "url": "/luiza-confeitaria/static/media/pexels-freestocksorg-867464.3a817ddc.jpg"
+  },
+  {
+    "revision": "133df60519cca106e489f5b02ce11790",
+    "url": "/luiza-confeitaria/static/media/pexels-trang-doan-1128678.133df605.jpg"
+  },
+  {
+    "revision": "4c3a69805f4590aa75c2c85c89d49728",
+    "url": "/luiza-confeitaria/static/media/right-arrow.4c3a6980.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/luiza-confeitaria/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/luiza-confeitaria/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/luiza-confeitaria/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/luiza-confeitaria/static/media/slick.f97e3bbf.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/luiza-confeitaria/precache-manifest.7c41d27090d86509e587e7b421fd61be.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/luiza-confeitaria/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
